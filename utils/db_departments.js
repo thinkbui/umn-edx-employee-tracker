f5038cf7a7c859ff122fd03113acedfc3fca6cae
Ian Bui <thinkbui@gmail.com>
2023-05-07 04:03:40 -0500

    Commented db_departments.js

--- utils/db_departments.js
+++ utils/db_departments.js
@@ -1,6 +1,7 @@
 const db_connection = require("./db_connection");
 const cTable = require('console.table');
 
+// This function prints the list of all departments
 function viewDepartments(return_func) {
   db_connection.db.query('SELECT * FROM departments', function (err, results) {
     let parsed_results = cTable.getTable(results);
@@ -9,6 +10,8 @@ function viewDepartments(return_func) {
   });
 }
 
+// This function adds a new department after prompting the user for the
+// information needed to create it
 async function addDepartment(inquirer, return_func) {
   await inquirer
     .prompt([
